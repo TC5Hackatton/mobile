21264228ac841e6ae3f9e29c9b5da81445ae98a7
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2026-01-30 07:15:02 -0300

    refactor: remove firebase example from Home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,20 @@
 import { StyleSheet, View } from 'react-native';
+import { Text } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import firebaseConfig from '@/firebaseConfig';
 import { logger } from '@/src/infrastructure/logger';
 import { AppHeader } from '@/src/presentation/components/app-header';
 import { ContentCard } from '@/src/presentation/components/content-card';
 import { FloatingActionButton } from '@/src/presentation/components/floating-action-button';
 import { customColors } from '@/src/presentation/constants/paper-theme';
 import { useAppHeader } from '@/src/presentation/hooks/use-app-header';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { useState } from 'react';
-import { Text } from 'react-native-paper';
 
 export default function HomeScreen() {
   const { handleExpandPress, handleThemeTogglePress } = useAppHeader();
-  const [testUser, setTestUser] = useState<string>('');
 
   const handleFABPress = () => {
     logger.log('FAB pressed');
     // TODO: Implementar ação do FAB
-
-    // TODO: Remover, só estamos provando que o Firebase funciona
-    signInWithEmailAndPassword(firebaseConfig.auth, 'teste@teste.com', 'senha123').then((value) => {
-      console.log(value.user);
-      setTestUser(`Email: ${value.user.email} \n\n\n UID: ${value.user.uid}`);
-    });
   };
 
   return (
@@ -33,7 +23,9 @@ export default function HomeScreen() {
         <AppHeader onExpandPress={handleExpandPress} onThemeTogglePress={handleThemeTogglePress} />
 
         <View style={styles.content}>
-          <ContentCard>{testUser && <Text>{testUser}</Text>}</ContentCard>
+          <ContentCard>
+            <Text>Welcome, user!</Text>
+          </ContentCard>
         </View>
 
         <FloatingActionButton
