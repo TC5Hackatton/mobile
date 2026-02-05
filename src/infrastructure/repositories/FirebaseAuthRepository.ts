453cabddf70a1dfd9476f0a879341376acfb3a6f
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2026-02-05 08:06:26 -0300

    chore: add some future TODOs

--- src/infrastructure/repositories/FirebaseAuthRepository.ts
+++ src/infrastructure/repositories/FirebaseAuthRepository.ts
@@ -3,7 +3,7 @@ import { AuthRepository, User } from '@/src/domain';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 export class FirebaseAuthRepository implements AuthRepository {
-  // TODO: decidir se eu vou criar mappers e DTOs, ou se não vai ser necessário
+  // TODO: decidir se eu vou criar mappers e DTOs, ou se não vai ser necessário (e então remover o "any")
   async signUp(email: string, password: string): Promise<any> {
     const firebaseResponse = await createUserWithEmailAndPassword(firebaseConfig.auth, email, password);
     return firebaseResponse.user;
