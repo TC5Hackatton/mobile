42861b94f18bcd9bc73a0dbcc70d4eb6366b776b
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2026-02-06 19:05:59 -0300

    feat: create DependenciesContext to share dependencies using Context API

---
+++ src/presentation/contexts/DependenciesContext.tsx
@@ -0,0 +1,36 @@
+import { AuthRepository, LoggerRepository } from '@/src/domain';
+import { FirebaseAuthRepository, InMemoryLoggerRepository } from '@/src/infrastructure';
+import { createContext, useContext, useMemo } from 'react';
+
+export interface AppDependencies {
+  authRepository: AuthRepository;
+  logger: LoggerRepository;
+}
+
+// Create a context for all the app dependencies
+const DependenciesContext = createContext<AppDependencies | null>(null);
+
+// Component (Provider) that provides all the dependencies instantiated through context
+export function DependenciesProvider({ children }: { children: React.ReactNode }) {
+  // NOTE: useMemo is essential here to avoid re-instantiating the dependencies on every render
+  const dependencies = useMemo<AppDependencies>(
+    () => ({
+      authRepository: new FirebaseAuthRepository(),
+      logger: new InMemoryLoggerRepository(),
+    }),
+    [],
+  );
+
+  return <DependenciesContext.Provider value={dependencies}>{children}</DependenciesContext.Provider>;
+}
+
+// Custom hook to use all the dependencies
+export const useDependencies = () => {
+  const dependencies = useContext(DependenciesContext);
+
+  if (!dependencies) {
+    throw new Error('useDependencies must be used within DependenciesProvider');
+  }
+
+  return dependencies;
+};
