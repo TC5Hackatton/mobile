4836c02b36e46347bffeac88638837758e2a117c
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2026-02-09 22:43:21 -0300

    feat: implement CreateTask use case - WIP

--- src/domain/usecases/task/CreateTaskUseCase.ts
+++ src/domain/usecases/task/CreateTaskUseCase.ts
@@ -1,7 +1,21 @@
-export class CreateTaskUseCase {
-  constructor() {}
+import { CreateTaskDTO } from '@/src/data';
+import { AuthRepository } from '../../repositories/AuthRepository';
+import { TaskRepository } from '../../repositories/TaskRepository';
 
-  execute() {
-    throw new Error('Method not implemented.');
+export class CreateTaskUseCase {
+  constructor(
+    private readonly authRepository: AuthRepository,
+    private readonly taskRepository: TaskRepository,
+  ) {}
+
+  async execute(task: CreateTaskDTO) {
+    // TODO: eu deveria ter uma store com o user armazenado quando ele estiver logado, e pegar de lá? (Boa pergunta para grupo de estudos)
+    const user = await this.authRepository.getCurrentUser();
+
+    if (!user?.uid) {
+      throw new Error('User is missing');
+    }
+
+    return this.taskRepository.createTask(task, user.uid);
   }
 }
