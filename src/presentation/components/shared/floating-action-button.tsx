2d1d0c6e6d4eb800c327b7a246a01f25fa17351c
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2026-02-14 10:26:08 -0300

    refactor: simplify FAB button

--- src/presentation/components/shared/floating-action-button.tsx
+++ src/presentation/components/shared/floating-action-button.tsx
@@ -1,81 +1,31 @@
 import { customColors } from '@/src/presentation/constants/paper-theme';
 import { spacing } from '@/src/presentation/constants/spacing';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { useRouter } from 'expo-router';
-import { useState } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import { FAB, Portal } from 'react-native-paper';
+import { router } from 'expo-router';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { AnimatedFAB } from 'react-native-paper';
 
 export function FloatingActionButton() {
-  const router = useRouter();
-  const [open, setOpen] = useState(false);
-
   return (
-    <Portal>
-      <View style={styles.wrapper}>
-        {open && (
-          <View style={styles.actions}>
-            {/* Checklist */}
-            <Pressable
-              style={styles.pillButton}
-              onPress={() => {
-                setOpen(false);
-                console.log('Checklist');
-              }}>
-              <MaterialCommunityIcons name="format-list-checks" size={20} color="#4A617C" />
-              <Text style={styles.pillText}>Checklist</Text>
-            </Pressable>
-
-            {/* Tarefa */}
-            <Pressable
-              style={styles.pillButton}
-              onPress={() => {
-                setOpen(false);
-                router.push('/task-creation');
-              }}>
-              <MaterialCommunityIcons name="file-document-outline" size={20} color="#4A617C" />
-              <Text style={styles.pillText}>Tarefa</Text>
-            </Pressable>
-          </View>
-        )}
-
-        <FAB icon={open ? 'close' : 'plus'} onPress={() => setOpen(!open)} style={styles.fab} color="#fff" />
-      </View>
-    </Portal>
+    <AnimatedFAB
+      icon="plus"
+      label=""
+      extended={false}
+      onPress={() => router.push('/task-creation')}
+      visible={true}
+      animateFrom="right"
+      iconMode="static"
+      style={styles.fabStyle}
+      color="#fff"
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
+  fabStyle: {
     position: 'absolute',
-    right: spacing.md,
     bottom: spacing.md,
-    alignItems: 'flex-end',
-  },
-
-  actions: {
-    marginBottom: 12,
-    gap: 10,
-  },
-
-  pillButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#BDD1E6',
-    borderRadius: 999,
-    paddingHorizontal: 16,
-    height: 42,
-    gap: 10,
-  },
-
-  pillText: {
-    color: '#4A617C',
-    fontSize: 15,
-    fontWeight: '600',
-  },
-
-  fab: {
+    right: spacing.md,
     backgroundColor: customColors.mediumBlue,
-    borderRadius: 28,
   },
 });
